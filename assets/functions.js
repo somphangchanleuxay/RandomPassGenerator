406914002299ffe648478bc362b23271f3a37d21
somphangchanleuxay <somphang.chanleuxay@gmail.com>
2023-11-16 13:34:54 -0800

    had to remodify code to meet criteria of the project, added  if statements to validate password restrictions, and prompts for USER to select to include into their password into the function of the code.

--- assets/functions.js
+++ assets/functions.js
@@ -1,35 +1,49 @@
-var length = 12;
-
-var upperCase = "ABCDEFGHILJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var number = "0123456789";
-var specialChar = "`~!@#$%^&*()-_=+{[}]<>/";
-
-var everyChar = upperCase + lowerCase + number + specialChar;
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input+
 function writePassword() {
-  var password = "";
-  password += upperCase[Math.floor(Math.random() * upperCase.length)];
-  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-  password += number[Math.floor(Math.random() * number.length)];
-  password += specialChar[Math.floor(Math.random() * specialChar.length)];
+  var length = prompt("Enter the length of the password");
   
-  while(length > password.length){
-    password += everyChar[Math.floor(Math.random() * everyChar.length)];
+  
+  if (isNaN(length) || length <= 8) {
+    alert("Please enter a valid number greater than 8 for the password length no more than 128");
+    return;
   }
 
+  var useUppercase = confirm("Include uppercase letters?");
+  var useLowercase = confirm("Include lowercase letters?");
+  var useNumbers = confirm("Include numbers?");
+  var useSpecialChars = confirm("Include special characters?");
+  
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var numberChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+[]{}|;:,.<>?";
 
+  var everyChar = "";
+  if (useUppercase) everyChar += uppercaseChars;
+  if (useLowercase) everyChar += lowercaseChars;
+  if (useNumbers) everyChar += numberChars;
+  if (useSpecialChars) everyChar += specialChars;
+
+  if (everyChar === "") {
+    alert("Please select at least one character set for the password.");
+    return;
+  }
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * everyChar.length);
+    password += everyChar.charAt(randomIndex);
 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+  }
+
 }
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
